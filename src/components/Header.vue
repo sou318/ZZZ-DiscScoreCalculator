221b2dcd8da25076aa750452f109a91a33a67ea9
sou <sou.suke.i0318@gmail.com>
2024-09-26 18:08:17 +0900

    implemented the header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text">
+        ZZZ - Disc Score Calculator
+    </div>
+</template>
+
+<style>
+    .text {
+        text-align: center;
+        padding: 0.5vh;
+        font-size: 2.5vh;
+        background-color: #fff1;
+        user-select: none;
+    }
+</style>
