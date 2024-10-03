2dab6c1fe2811d07199e3154e612a9c1688e8058
sou <sou.suke.i0318@gmail.com>
2024-10-03 21:16:17 +0900

    ヘッダーからGitHubに飛べるように

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,25 @@
 <template>
     <div class="header">
-        ZZZ - Disc Score Calculator
+        <a href="https://github.com/sou318/ZZZ-DiscScoreCalculator">ZZZ - Disc Score Calculator</a>
     </div>
 </template>
 
-<style scoped>
-    .header {
-        border-bottom: thin solid black;
-        text-align: center;
-        padding: 0.5vh;
-        font-size: 1.5rem;
-        background-color: #fff1;
+<style scoped lang="scss">
+.header {
+    border-bottom: thin solid black;
+    text-align: center;
+    padding: 0.5vh;
+    font-size: 1.5rem;
+    background-color: #fff1;
+    transition: 100ms;
+
+    a {
+        color: white;
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
     }
+}
 </style>
